d99c4e8e7aadc72be098019dfe07fa39be30bd59
ngzuan <nvtuan181102@gmail.com>
2023-04-24 23:27:51 +0700

    loops

---
+++ loops.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	// loop bthuong
+	// for i := 0; i <= 10; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	//lap ..
+	// name := []string{"tuan", "nguyen", "van", "la", "hihi"}
+	// for i, myname := range name {
+	// 	fmt.Println(i, myname)
+	// }
+
+	// animal := make(map[string]string)
+	// animal["dog"] = "cun"
+	// animal["cat"] = "meo meo"
+	// for i, myname := range animal {
+	// 	fmt.Println(i, myname)
+	// }
+	// result dog cun
+	// 		  cat meo meo
+
+	var text string = "tuan dep trai vai loz"
+	for i, t := range text {
+		fmt.Println(i, t)
+	}
+}
